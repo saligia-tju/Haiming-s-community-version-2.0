f61663f48bcda9107e28fa26474c2ab368dc16ef
saligia-tju <hmyang_tju@163.com>
2021-06-08 23:37:56 +0800

    add publish web

---
+++ src/main/java/life/haiming/community/controller/PublishController.java
@@ -0,0 +1,12 @@
+package life.haiming.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+}
