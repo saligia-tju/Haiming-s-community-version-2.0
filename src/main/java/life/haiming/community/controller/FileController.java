33f43892cebeb64b6056e1a670b7598d622cba97
saligia-tju <hmyang_tju@163.com>
2021-06-30 17:58:33 +0800

    Add picture upload function

---
+++ src/main/java/life/haiming/community/controller/FileController.java
@@ -0,0 +1,20 @@
+package life.haiming.community.controller;
+
+import life.haiming.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload() {
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/wechat.jpg");
+
+        return fileDTO;
+    }
+}
+
---
+++ src/main/java/life/haiming/community/dto/FileDTO.java
@@ -0,0 +1,10 @@
+package life.haiming.community.dto;
+
+import lombok.Data;
+
+@Data
+public class FileDTO {
+    private int success;
+    private String message;
+    private String url;
+}
